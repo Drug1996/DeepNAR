23534e91d77034126927ae0c47207d95d9b56e5a
zhong <zhong@race.u-tokyo.ac.jp>
2019-05-07 12:17:27 +0900

    Divide data set into three parts: training, dev and test.

--- main.py
+++ main.py
@@ -19,30 +19,29 @@ input_size = 36  # including 6 channels of 6 IMU sensors, totally 36 channels
 hidden_size = 64  # parameters for LSTM (Long Short Term Memory)
 num_layers = 2  # the depth of Deep-RNNs
 num_classes = 3
-batch_size = 45
-num_epochs = 150
+batch_size = 18
+num_epochs = 10
 learning_rate = 0.1
-training_test_ratio = 0.75
+training_dev_test_ratio = [0.6, 0.5]
 dimension_interval = 1
 
 # Test parameters
-TEST_NUM = 4
+TEST_NUM = 1
 RANDOM_SEED_NUM = 7
-PYTORCH_SEED_NUM = list(range(4, 5))
+PYTORCH_SEED_NUM = list(range(5))
 LABELS_NAME = ['None', 'Right turning', 'Wrong turning']
 
 
 # Load the data sub-network
-def dataloader(data_type='standing', training_test_ratio=0.75):
+def dataloader(data_type='standing', training_dev_test_ratio=[0.6, 0.5]):
     # fix the random seed
-    # random.seed(RANDOM_SEED_NUM)
+    random.seed(RANDOM_SEED_NUM)
 
     X = []
     Y = []
     pkl_file = open(DATASET_NAME, 'rb')
     dataset = pickle.load(pkl_file)
     dataset_size = len(dataset)
-    # print(dataset_size)
 
     MAX_T = 0
     for i in range(dataset_size):
@@ -69,33 +68,46 @@ def dataloader(data_type='standing', training_test_ratio=0.75):
                     Y.append(2)
 
     # divide the whole dataset as training dataset and test dataset
-    index = [i for i in range(len(X))]
-    index_0_ori = [i for i in index if Y[i] == 0]
-    index_0 = random.sample(index_0_ori, int(len(index_0_ori) * 0.5))
+    index = list(range(dataset_size))
+
+    # Get training index
+    index_0 = [i for i in index if Y[i] == 0]
+    index_0 = random.sample(index_0, int(len(index_0) * 0.5))  # reduce the number of class 0
     index_1 = [i for i in index if Y[i] == 1]
     index_2 = [i for i in index if Y[i] == 2]
-
-    # check the length of index_0 - index_2
     index_0_size, index_1_size, index_2_size = len(index_0), len(index_1), len(index_2)
-    # print(index_0_size, index_1_size, index_2_size)
-
     training_index = []
-    training_index += random.sample(index_0, int(index_0_size*training_test_ratio))
-    training_index += random.sample(index_1, int(index_1_size*training_test_ratio))
-    training_index += random.sample(index_2, int(index_2_size*training_test_ratio))
-    test_index = []
-    for num in index_0 + index_1 + index_2:
-        if num not in training_index:
-            test_index.append(num)
+    training_index += random.sample(index_0, int(index_0_size*training_dev_test_ratio[0]))
+    training_index += random.sample(index_1, int(index_1_size*training_dev_test_ratio[0]))
+    training_index += random.sample(index_2, int(index_2_size*training_dev_test_ratio[0]))
+    random.shuffle(training_index)  # shuffle the training data
+
+    # Get dev index
+    index_0 = [num for num in index_0 if num not in training_index]
+    index_1 = [num for num in index_1 if num not in training_index]
+    index_2 = [num for num in index_2 if num not in training_index]
+    index_0_size, index_1_size, index_2_size = len(index_0), len(index_1), len(index_2)
+    dev_index = []
+    dev_index += random.sample(index_0, int(index_0_size*training_dev_test_ratio[1]))
+    dev_index += random.sample(index_1, int(index_1_size * training_dev_test_ratio[1]))
+    dev_index += random.sample(index_2, int(index_2_size * training_dev_test_ratio[1]))
+
+    # Get test index
+    index_0 = [num for num in index_0 if num not in dev_index]
+    index_1 = [num for num in index_1 if num not in dev_index]
+    index_2 = [num for num in index_2 if num not in dev_index]
+    test_index = index_0 + index_1 + index_2
+
     # Data selection and dimension reduction
     X_training = [dimension_reduce(X[i], dimension_interval) for i in training_index]
     Y_training = [Y[i] for i in training_index]
+    X_dev = [dimension_reduce(X[i], dimension_interval) for i in dev_index]
+    Y_dev = [Y[i] for i in dev_index]
     X_test = [dimension_reduce(X[i], dimension_interval) for i in test_index]
     Y_test = [Y[i] for i in test_index]
 
     # update the value of max sequence length after dimension reduction
     MAX_T = X_training[0].shape[0]
-    # renew the parameter of sequence length
     global sequence_length
     sequence_length = MAX_T
 
@@ -104,20 +116,41 @@ def dataloader(data_type='standing', training_test_ratio=0.75):
     # X_test = normalization(X_test)
 
     # change the raw data form to pytorch data form
-    X_training, Y_training, X_test, Y_test = np.array(X_training), np.array(Y_training), np.array(X_test), np.array(Y_test)
+    X_training, Y_training = np.array(X_training), np.array(Y_training)
+    X_dev, Y_dev = np.array(X_dev), np.array(Y_dev)
+    X_test, Y_test = np.array(X_test), np.array(Y_test)
 
-    # check the dimensions
+    # # check the dimensions
     # print(X_training.shape)
     # print(Y_training.shape)
+    # print(X_dev.shape)
+    # print(Y_dev.shape)
     # print(X_test.shape)
     # print(Y_test.shape)
 
-    X_training, Y_training, X_test, Y_test = torch.from_numpy(X_training), torch.from_numpy(Y_training), torch.from_numpy(X_test), torch.from_numpy(Y_test)
-    return X_training.type(torch.FloatTensor), Y_training.type(torch.LongTensor), X_test.type(torch.FloatTensor), Y_test.type(torch.LongTensor)
+    X_training, Y_training = torch.from_numpy(X_training), torch.from_numpy(Y_training)
+    X_dev, Y_dev = torch.from_numpy(X_dev), torch.from_numpy(Y_dev)
+    X_test, Y_test = torch.from_numpy(X_test), torch.from_numpy(Y_test)
+
+    return X_training.type(torch.FloatTensor), Y_training.type(torch.LongTensor),\
+           X_dev.type(torch.FloatTensor), Y_dev.type(torch.LongTensor),\
+           X_test.type(torch.FloatTensor), Y_test.type(torch.LongTensor)
 
 
-def main(SEED):
-    torch.manual_seed(SEED)
+def main(num):
+    # torch.manual_seed(SEED)
+
+    # Create the model
+    model = BiRNN(input_size, hidden_size, num_layers, num_classes).to(device)
+
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Load the dataset
+    X_training, Y_training, X_dev, Y_dev, X_test, Y_test = \
+        dataloader(data_type='turning', training_dev_test_ratio=training_dev_test_ratio)
+
     training_losseslist = []
     test_accuracieslist = []
     training_losses = []
@@ -131,21 +164,16 @@ def main(SEED):
         try:
             training_losses = []
             test_accuracies = []
-            y_true = []
-            y_pred = []
-            X_training, Y_training, X_test, Y_test = dataloader(data_type='turning', training_test_ratio=training_test_ratio)
-
-            model = BiRNN(input_size, hidden_size, num_layers, num_classes).to(device)
-            # Loss and optimizer
-            criterion = nn.CrossEntropyLoss()
-            optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
             # Train the model
             X_training = X_training.reshape(-1, batch_size, sequence_length, input_size)
             Y_training = Y_training.reshape(-1, batch_size)
+            X_dev = X_test.reshape(-1, 1, sequence_length, input_size)
+            Y_dev = Y_test.reshape(-1, 1)
             X_test = X_test.reshape(-1, 1, sequence_length, input_size)
             Y_test = Y_test.reshape(-1, 1)
-            total_step = len(X_training)
+
+            total_step = len(X_training)  # how many batches for one epoch
             for epoch in range(num_epochs):
                 for i in range(total_step):
                     inputs = X_training[i]
@@ -156,7 +184,6 @@ def main(SEED):
 
                     # Forward pass
                     outputs = model(inputs)
-                    #print(outputs.shape, labels.data)
                     training_loss = criterion(outputs, labels)
 
                     # Backward and optimize
@@ -164,31 +191,33 @@ def main(SEED):
                     training_loss.backward()
                     optimizer.step()
 
-                    if (epoch + 1) % 5 == 0:
-                        print('Trials [{}/{}], Epoch [{}/{}]], Loss: {:.4f}'
-                            .format(t + 1, TEST_NUM, epoch + 1, num_epochs, training_loss.item()))
+                if (epoch + 1) % 5 == 0:
+                    print('Trials [{}/{}], Epoch [{}/{}]], Loss: {:.4f}'
+                          .format(t + 1, TEST_NUM, epoch + 1, num_epochs, training_loss.item()))
 
-                        training_losses.append(training_loss.item())
+                    # Get the value of loss
+                    training_losses.append(training_loss.item())
 
-                        # Test the model
-                        with torch.no_grad():
-                            y_true.clear()
-                            y_pred.clear()
-                            correct = 0
-                            total = 0
-                            for j in range(len(X_test)):
-                                inputs = X_test[j]
-                                inputs = inputs.reshape(-1, sequence_length, input_size).to(device)
-                                labels = Y_test[j]
-                                labels = labels.reshape(-1)
-                                labels = labels.to(device)
-                                outputs = model(inputs)
-                                _, predicted = torch.max(outputs.data, 1)
-                                total += labels.size(0)
-                                correct += (predicted == labels).sum().item()
-                                y_true.append(labels.item())
-                                y_pred.append(predicted.item())
-                            test_accuracies.append(correct/total)
+                    # Test the model on dev set
+                    with torch.no_grad():
+                        y_true = []
+                        y_pred = []
+                        correct = 0
+                        total = 0
+                        for j in range(len(X_dev)):
+                            inputs = X_dev[j]
+                            inputs = inputs.reshape(-1, sequence_length, input_size).to(device)
+                            labels = Y_dev[j]
+                            labels = labels.reshape(-1)
+                            labels = labels.to(device)
+                            outputs = model(inputs)
+                            _, predicted = torch.max(outputs.data, 1)
+                            total += labels.size(0)
+                            correct += (predicted == labels).sum().item()
+                            y_true.append(labels.item())
+                            y_pred.append(predicted.item())
+                        test_accuracies.append(correct/total)
+
         except KeyboardInterrupt:
             print('Stop!')
 
@@ -196,12 +225,6 @@ def main(SEED):
         test_accuracieslist.append(test_accuracies)
         y_truelist.append(y_true)
         y_predlist.append(y_pred)
-
-        # print(t)
-        # print(training_losseslist)
-        # print(test_accuracieslist)
-        # print(y_truelist)
-        # print(y_predlist)
 
     # Print accuracy of the model
     accuracy = 0
@@ -216,12 +239,11 @@ def main(SEED):
 
     # Show or save the graph of variance and bias analysis, and confusion matrix graph
     variance_and_bias_analysis(training_losseslist=training_losseslist, test_accuracieslist=test_accuracieslist)
-    save('ran_seed_' + str(RANDOM_SEED_NUM) + '_loss_accuracy' + '.png')
+    save('trials' + str(num) + '_loss_accuracy' + '.png')
     plot_confusion_matrix(y_truelist=y_truelist, y_predlist=y_predlist, labels_name=LABELS_NAME)
-    save('ran_seed_' + str(RANDOM_SEED_NUM) + '_confusion_matrix' + '.png')
+    save('trials_' + str(num) + '_confusion_matrix' + '.png')
 
 
 if __name__ == '__main__':
     for num in PYTORCH_SEED_NUM:
         main(num)
-    # data_visual()
